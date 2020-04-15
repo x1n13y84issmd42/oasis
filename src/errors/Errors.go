c7153d16ea7618a3dc5ac00038623c695134b0fa
Eugene M <gaddeena@gmail.com>
2020-04-15 14:06:22 +0300

    Comments.

--- src/errors/Errors.go
+++ src/errors/Errors.go
@@ -3,25 +3,30 @@ package errors
 import "github.com/x1n13y84issmd42/oasis/src/strings"
 
 // Base is a generic error used within Oasis.
-// It references it's root cause.
+// Having it's root cause is what makes it good.
 type Base struct {
 	TheCause error
 }
 
+// IError is an error interface.
 type IError interface {
 	Error() string
 	Cause() error
 }
 
+// Error return a string representation of an error.
 func (err Base) Error() string {
 	return "---"
 }
 
+// Cause return an cause error instance.
 func (err Base) Cause() error {
 	return err.TheCause
 }
 
-// ErrNoParameters ...
+// ErrNoParameters happens when there are parameters missing
+// which are needed to build an operation before testing.
+// Usually happens in paths, queries & headers.
 type ErrNoParameters struct {
 	Base
 	MissingParams []string
@@ -39,7 +44,7 @@ func (err ErrNoParameters) Error() string {
 	return "Parameters " + strings.EnumJoin(missing) + " are missing their values."
 }
 
-// NoParameters ...
+// NoParameters creates a new ErrNoParameters error instance.
 func NoParameters(missingParams []string, cause error) ErrNoParameters {
 	return ErrNoParameters{
 		Base: Base{
