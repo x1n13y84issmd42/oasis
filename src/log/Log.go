81bd32ec5007740074fccaebcb9259c04409ea85
Eugene M <gaddeena@gmail.com>
2020-04-29 17:09:57 +0300

    Documented the Log.

--- src/log/Log.go
+++ src/log/Log.go
@@ -74,12 +74,13 @@ func (log Log) Usage() {
 	fmt.Println("oasis from path/to/oas_spec.yaml test operation_id")
 }
 
-// Error --
+// Error outputs errors. It accepts both built-in errors as well as errors.IError instances.
+// The latter carry their cause error, if any, and those will be recursively printed.
 func (log Log) Error(err error) {
 	log.XError(err, log.Style, Tab(0))
 }
 
-// LoadingSpec --
+// LoadingSpec informs about the API specification being used.
 func (log Log) LoadingSpec(path string) {
 	log.Println(2, "Loading %s", log.Style.URL(path))
 }
@@ -100,22 +101,22 @@ func (log Log) PrintOperations(ops []*api.Operation) {
 	}
 }
 
-// TestingProject --
+// TestingProject informs about the project being tested.
 func (log Log) TestingProject(pi *api.ProjectInfo) {
 	log.Println(2, "Testing the %s @ %s", log.Style.Op(pi.Title), log.Style.ID(pi.Version))
 }
 
-// UsingHost --
+// UsingHost informs about the API host being used for testing.
 func (log Log) UsingHost(host *api.Host) {
 	log.Println(2, "Using the %s host @ %s", log.Style.Op(host.Name), log.Style.URL(host.URL))
 }
 
-// UsingDefaultHost --
+// UsingDefaultHost informs that a default host has been chosen fr testing.
 func (log Log) UsingDefaultHost() {
 	log.Println(2, "No host name has been specified, using the first one in the list.")
 }
 
-// HostNotFound ...
+// HostNotFound informs that no specified host has been found in the spec.
 func (log Log) HostNotFound(h string) {
 	if h == "" {
 		log.Println(2, "No default host is found in the spec.")
@@ -124,37 +125,37 @@ func (log Log) HostNotFound(h string) {
 	}
 }
 
-// UsingSecurity --
+// UsingSecurity informs about security mechanisms being used during testing.
 func (log Log) UsingSecurity(sec api.ISecurity) {
 	log.Println(3, "\tUsing the %s security settings.", log.Style.ID(sec.GetName()))
 }
 
-// SecurityHasNoData --
+// SecurityHasNoData informs that the selected security settings has no data to use in requests.
 func (log Log) SecurityHasNoData(sec api.ISecurity) {
 	log.Println(3, "\tThe security %s contains no data to use in request.", log.Style.ID(sec.GetName()))
 }
 
-// Requesting --
+// Requesting informs about an HTTP request being performed.
 func (log Log) Requesting(method string, URL string) {
 	log.Println(2, "\tRequesting %s @ %s", log.Style.Method(method), log.Style.URL(URL))
 }
 
-// ParameterHasNoExample --
+// ParameterHasNoExample informs that a parameter has no example value to use.
 func (log Log) ParameterHasNoExample(paramName string, in string, container string) {
 	log.Println(5, "\tThe %s parameter %s (from %s) has no example value to use.", in, log.Style.ID(paramName), container)
 }
 
-// UsingParameterExample --
+// UsingParameterExample informs that a parameter example being used.
 func (log Log) UsingParameterExample(paramName string, in string, container string) {
 	log.Println(5, "\tUsing the %s parameter %s (from %s) example.", in, log.Style.ID(paramName), container)
 }
 
-// HeaderHasNoValue --
+// HeaderHasNoValue informs that a required response header has no data.
 func (log Log) HeaderHasNoValue(hdr *api.Header) {
 	log.Println(1, "\tHeader \"%s\" is required but is not present.", hdr.Name)
 }
 
-// ResponseHasWrongStatus --
+// ResponseHasWrongStatus informs that the received response has wrong/unexpected status.
 func (log Log) ResponseHasWrongStatus(resp *api.Response, actualStatus int) {
 	m := strings.Join([]string{
 		"\t",
@@ -166,7 +167,7 @@ func (log Log) ResponseHasWrongStatus(resp *api.Response, actualStatus int) {
 	log.Println(2, m, log.Style.ValueExpected(resp.StatusCode), log.Style.ValueActual(actualStatus))
 }
 
-// ResponseHasWrongContentType --
+// ResponseHasWrongContentType informs that the received response has wrong/unexpected Content-Type header value.
 func (log Log) ResponseHasWrongContentType(resp *api.Response, actualCT string) {
 	m := strings.Join([]string{
 		"\t",
@@ -179,46 +180,45 @@ func (log Log) ResponseHasWrongContentType(resp *api.Response, actualCT string) 
 	log.Println(2, m, log.Style.ValueExpected(resp.ContentType), log.Style.ValueActual(actualCT))
 }
 
-// TestingOperation --
+// TestingOperation informs about an operation being tested.
 func (log Log) TestingOperation(op *api.Operation) {
 	log.Print(1, "Testing the %s operation... ", log.Style.Op(op.Name))
 	log.Print(2, "\n")
 }
 
-// OperationOK --
+// OperationOK informs that the operation has finished successfully.
 func (log Log) OperationOK(res *api.Operation) {
 	log.Print(2, "\t")
 	log.Println(1, "%s", log.Style.OK("SUCCESS"))
 	log.Print(2, "\n")
 }
 
-// OperationFail --
+// OperationFail informs that the operation has failed.
 func (log Log) OperationFail(res *api.Operation) {
 	log.Print(2, "\t")
 	log.Println(1, "%s", log.Style.Failure("FAILURE"))
 	log.Print(2, "\n")
 }
 
-// OperationNotFound --
+// OperationNotFound informs that the requested operation is not found in the spec.
 func (log Log) OperationNotFound(op string) {
 	log.Println(1, "The operation \"%s\" isn't there.", op)
 }
 
-// SchemaTesting --
+// SchemaTesting informs about a value being tested againt some JSON schema.
 func (log Log) SchemaTesting(schema *api.Schema, data interface{}) {
 	datas := log.Style.ValueActual(fmt.Sprintf("%#v", data))
 	log.Print(4, "\t%s: testing %s", log.Style.ID(schema.Name), datas)
 }
 
-// SchemaOK --
+// SchemaOK informs that JSON schema testing finished successfully.
 func (log Log) SchemaOK(schema *api.Schema) {
 	log.Println(4, log.Style.Success(" - OK"))
 }
 
-// SchemaFail --
+// SchemaFail informs that JSON schema testing finished unsuccessfully.
 func (log Log) SchemaFail(schema *api.Schema, errors []gojsonschema.ResultError) {
 	log.Println(4, log.Style.Error(" - FAILURE"))
-	// log.Println(4, "\tSchema \"%s\" has errors.", schema.Name)
 
 	for _, desc := range errors {
 		log.Println(4, "\t\t%s", log.Style.Error(desc))
