c88c9a15e0a05abbf732f7fd95aa30f7cf4947fd
Eugene M <gaddeena@gmail.com>
2020-04-29 17:10:07 +0300

    Added a test.JSONResponse() test.

--- src/test/JSONResponse.go
+++ src/test/JSONResponse.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -10,40 +9,38 @@ import (
 	"github.com/x1n13y84issmd42/oasis/src/log"
 )
 
-// JSONResponse test response bodies.
+// JSONResponse tests JSON response bodies.
 func JSONResponse(resp *http.Response, specResp *api.Response, logger log.ILogger) bool {
 	if resp.Body == nil {
 		return false
 	}
 
 	respData, _ := ioutil.ReadAll(resp.Body)
-	// fmt.Println("JSONResponseString body: ", string(respData))
-	// fmt.Println("JSONResponseString status: ", resp.StatusCode)
 
 	var err error = nil
 
-	if res, err := tryJSONObjectResponse(&respData, specResp, logger); err == nil {
+	if res, err := TryJSONObjectResponse(&respData, specResp, logger); err == nil {
 		return Schema(res, specResp.Schema, logger)
 	}
 
-	if res, err := tryJSONArrayResponse(&respData, specResp, logger); err == nil {
+	if res, err := TryJSONArrayResponse(&respData, specResp, logger); err == nil {
 		return Schema(res, specResp.Schema, logger)
 	}
 
-	if res, err := tryJSONStringResponse(&respData, specResp, logger); err == nil {
+	if res, err := TryJSONStringResponse(&respData, specResp, logger); err == nil {
 		return Schema(res, specResp.Schema, logger)
 	}
 
-	if res, err := tryJSONNumberResponse(&respData, specResp, logger); err == nil {
+	if res, err := TryJSONNumberResponse(&respData, specResp, logger); err == nil {
 		return Schema(res, specResp.Schema, logger)
 	}
 
-	if res, err := tryJSONBooleanResponse(&respData, specResp, logger); err == nil {
+	if res, err := TryJSONBooleanResponse(&respData, specResp, logger); err == nil {
 		return Schema(res, specResp.Schema, logger)
 	}
 
 	if err != nil {
-		fmt.Printf("JSONResponse Error: %s", err.Error())
+		logger.Error(err)
 	}
 
 	return false
@@ -57,26 +54,26 @@ type (
 	JSONArray = []interface{}
 )
 
-// tryJSONStringResponse tryes to unmarshal respData as a string.
-func tryJSONStringResponse(respData *[]byte, specResp *api.Response, logger log.ILogger) (res string, err error) {
+// TryJSONStringResponse tries to unmarshal respData as a string.
+func TryJSONStringResponse(respData *[]byte, specResp *api.Response, logger log.ILogger) (res string, err error) {
 	err = json.Unmarshal(*respData, &res)
 	return
 }
 
-// tryJSONNumberResponse tryes to unmarshal respData as a string.
-func tryJSONNumberResponse(respData *[]byte, specResp *api.Response, logger log.ILogger) (res int64, err error) {
+// TryJSONNumberResponse tries to unmarshal respData as a number.
+func TryJSONNumberResponse(respData *[]byte, specResp *api.Response, logger log.ILogger) (res int64, err error) {
 	err = json.Unmarshal(*respData, &res)
 	return
 }
 
-// tryJSONBooleanResponse tryes to unmarshal respData as a string.
-func tryJSONBooleanResponse(respData *[]byte, specResp *api.Response, logger log.ILogger) (res bool, err error) {
+// TryJSONBooleanResponse tries to unmarshal respData as a boolean.
+func TryJSONBooleanResponse(respData *[]byte, specResp *api.Response, logger log.ILogger) (res bool, err error) {
 	err = json.Unmarshal(*respData, &res)
 	return
 }
 
-// tryJSONObjectResponse tryes to unmarshal respData as a string.
-func tryJSONObjectResponse(respData *[]byte, specResp *api.Response, logger log.ILogger) (res *JSONMap, err error) {
+// TryJSONObjectResponse tries to unmarshal respData as an object.
+func TryJSONObjectResponse(respData *[]byte, specResp *api.Response, logger log.ILogger) (res *JSONMap, err error) {
 	v := make(JSONMap)
 	err = json.Unmarshal(*respData, &v)
 
@@ -90,8 +87,8 @@ func tryJSONObjectResponse(respData *[]byte, specResp *api.Response, logger log.
 	return
 }
 
-// tryJSONArrayResponse tryes to unmarshal respData as a string.
-func tryJSONArrayResponse(respData *[]byte, specResp *api.Response, logger log.ILogger) (res *JSONArray, err error) {
+// TryJSONArrayResponse tries to unmarshal respData as an array.
+func TryJSONArrayResponse(respData *[]byte, specResp *api.Response, logger log.ILogger) (res *JSONArray, err error) {
 	var v JSONArray
 	err = json.Unmarshal(*respData, &v)
 
---
+++ src/test/JSONResponse_test.go
@@ -0,0 +1,249 @@
+package test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/x1n13y84issmd42/oasis/src/api"
+	"github.com/x1n13y84issmd42/oasis/src/log"
+)
+
+func TestJSONResponse_String(T *testing.T) {
+	httpResp := &http.Response{
+		Body: ioutil.NopCloser(bytes.NewReader([]byte("\"anything works until it's quoted\""))),
+	}
+
+	specResp := &api.Response{
+		Schema: &api.Schema{
+			JSONSchema: api.JSONSchema{
+				"type": "string",
+			},
+		},
+	}
+
+	actual := JSONResponse(httpResp, specResp, log.NewFestive(0))
+	assert.True(T, actual)
+}
+
+func TestJSONResponse_String_False(T *testing.T) {
+	httpResp := &http.Response{
+		Body: ioutil.NopCloser(bytes.NewReader([]byte("unquoted malformed string\""))),
+	}
+
+	specResp := &api.Response{
+		Schema: &api.Schema{
+			JSONSchema: api.JSONSchema{
+				"type": "string",
+			},
+		},
+	}
+
+	actual := JSONResponse(httpResp, specResp, log.NewFestive(0))
+	assert.False(T, actual)
+}
+
+func TestJSONResponse_Number(T *testing.T) {
+	httpResp := &http.Response{
+		Body: ioutil.NopCloser(bytes.NewReader([]byte("42"))),
+	}
+
+	specResp := &api.Response{
+		Schema: &api.Schema{
+			JSONSchema: api.JSONSchema{
+				"type": "number",
+			},
+		},
+	}
+
+	actual := JSONResponse(httpResp, specResp, log.NewFestive(0))
+	assert.True(T, actual)
+}
+
+func TestJSONResponse_Number_False(T *testing.T) {
+	httpResp := &http.Response{
+		Body: ioutil.NopCloser(bytes.NewReader([]byte("not a number, not even close"))),
+	}
+
+	specResp := &api.Response{
+		Schema: &api.Schema{
+			JSONSchema: api.JSONSchema{
+				"type": "number",
+			},
+		},
+	}
+
+	actual := JSONResponse(httpResp, specResp, log.NewFestive(0))
+	assert.False(T, actual)
+}
+
+func TestJSONResponse_Boolean(T *testing.T) {
+	httpResp := &http.Response{
+		Body: ioutil.NopCloser(bytes.NewReader([]byte("false"))),
+	}
+
+	specResp := &api.Response{
+		Schema: &api.Schema{
+			JSONSchema: api.JSONSchema{
+				"type": "boolean",
+			},
+		},
+	}
+
+	actual := JSONResponse(httpResp, specResp, log.NewFestive(0))
+	assert.True(T, actual)
+}
+
+func TestJSONResponse_Boolean_False(T *testing.T) {
+	httpResp := &http.Response{
+		Body: ioutil.NopCloser(bytes.NewReader([]byte("truth"))),
+	}
+
+	specResp := &api.Response{
+		Schema: &api.Schema{
+			JSONSchema: api.JSONSchema{
+				"type": "boolean",
+			},
+		},
+	}
+
+	actual := JSONResponse(httpResp, specResp, log.NewFestive(0))
+	assert.False(T, actual)
+}
+
+func TestJSONResponse_Object(T *testing.T) {
+	httpResp := &http.Response{
+		Body: ioutil.NopCloser(bytes.NewReader([]byte("{\"name\":\"johnny\",\"age\":42}"))),
+	}
+
+	specResp := &api.Response{
+		Schema: &api.Schema{
+			JSONSchema: api.JSONSchema{
+				"type": "object",
+				"properties": map[string]api.JSONSchema{
+					"name": {
+						"type": "string",
+					},
+					"age": {
+						"type": "number",
+					},
+				},
+			},
+		},
+	}
+
+	actual := JSONResponse(httpResp, specResp, log.NewFestive(0))
+	assert.True(T, actual)
+}
+
+func TestJSONResponse_Object_False_Schema(T *testing.T) {
+	httpResp := &http.Response{
+		Body: ioutil.NopCloser(bytes.NewReader([]byte("{\"name\":false,\"age\":\"42\"}"))),
+	}
+
+	specResp := &api.Response{
+		Schema: &api.Schema{
+			JSONSchema: api.JSONSchema{
+				"type": "object",
+				"properties": map[string]api.JSONSchema{
+					"name": {
+						"type": "string",
+					},
+					"age": {
+						"type": "number",
+					},
+				},
+			},
+		},
+	}
+
+	actual := JSONResponse(httpResp, specResp, log.NewFestive(0))
+	assert.False(T, actual)
+}
+
+func TestJSONResponse_Object_False_Unmarshal(T *testing.T) {
+	httpResp := &http.Response{
+		Body: ioutil.NopCloser(bytes.NewReader([]byte("ame\":false,\"age\":\"42\"}"))),
+	}
+
+	specResp := &api.Response{
+		Schema: &api.Schema{
+			JSONSchema: api.JSONSchema{
+				"type": "object",
+				"properties": map[string]api.JSONSchema{
+					"name": {
+						"type": "string",
+					},
+					"age": {
+						"type": "number",
+					},
+				},
+			},
+		},
+	}
+
+	actual := JSONResponse(httpResp, specResp, log.NewFestive(0))
+	assert.False(T, actual)
+}
+
+func TestJSONResponse_Array(T *testing.T) {
+	httpResp := &http.Response{
+		Body: ioutil.NopCloser(bytes.NewReader([]byte("[1, 2, 3, 4]"))),
+	}
+
+	specResp := &api.Response{
+		Schema: &api.Schema{
+			JSONSchema: api.JSONSchema{
+				"type": "array",
+				"items": api.JSONSchema{
+					"type": "integer",
+				},
+			},
+		},
+	}
+
+	actual := JSONResponse(httpResp, specResp, log.NewFestive(0))
+	assert.True(T, actual)
+}
+
+func TestJSONResponse_Array_False_Schema(T *testing.T) {
+	httpResp := &http.Response{
+		Body: ioutil.NopCloser(bytes.NewReader([]byte("[\"1\", 2, \"3\", 4]"))),
+	}
+
+	specResp := &api.Response{
+		Schema: &api.Schema{
+			JSONSchema: api.JSONSchema{
+				"type": "array",
+				"items": api.JSONSchema{
+					"type": "integer",
+				},
+			},
+		},
+	}
+
+	actual := JSONResponse(httpResp, specResp, log.NewFestive(0))
+	assert.False(T, actual)
+}
+
+func TestJSONResponse_Array_False_Unmarshal(T *testing.T) {
+	httpResp := &http.Response{
+		Body: ioutil.NopCloser(bytes.NewReader([]byte("[1, 2, \"3', "))),
+	}
+
+	specResp := &api.Response{
+		Schema: &api.Schema{
+			JSONSchema: api.JSONSchema{
+				"type": "array",
+				"items": api.JSONSchema{
+					"type": "integer",
+				},
+			},
+		},
+	}
+
+	actual := JSONResponse(httpResp, specResp, log.NewFestive(0))
+	assert.False(T, actual)
+}
