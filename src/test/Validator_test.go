db952387bbaaf08c0b589370cbd409e5d4ba7635
Eugene M <gaddeena@gmail.com>
2020-07-03 12:58:55 +0300

    Validator test.

---
+++ src/test/Validator_test.go
@@ -0,0 +1,28 @@
+package test_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/x1n13y84issmd42/oasis/src/contract"
+	"github.com/x1n13y84issmd42/oasis/src/log"
+	"github.com/x1n13y84issmd42/oasis/src/test"
+)
+
+func Test_Validator(T *testing.T) {
+	log := log.NewPlain(0)
+	v := test.NewValidator(log)
+
+	v.Expect(func(req *http.Response, log contract.Logger) bool {
+		return false
+	})
+
+	r := &contract.OperationResult{
+		Success: true,
+	}
+
+	r = v.Validate(r)
+
+	assert.False(T, r.Success)
+}
